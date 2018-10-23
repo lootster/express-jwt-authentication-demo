2c215bcf6ea1379de8ac3b4395192807a108483c
lootster <sheldonkhlim@gmail.com>
2018-10-23 12:09:10 +0800

    create unique fields test cases

--- src/model/user.js
+++ src/model/user.js
@@ -1,16 +1,21 @@
 // in user.js
+const uniqueValidator = require("mongoose-unique-validator");
 
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
-    index: true
+    index: true,
+    unique: true
   },
   email: {
     type: String,
-    index: true
+    index: true,
+    unique: true
   }
 });
+
+UserSchema.plugin(uniqueValidator, { message: "should be unique" });
 
 module.exports = mongoose.model("User", UserSchema);
--- src/model/user.test.js
+++ src/model/user.test.js
@@ -1,3 +1,4 @@
+const ValidationError = require("mongoose").ValidationError;
 const test_mongodb = require("../../test_helper/in_memory_mongodb_setup");
 
 beforeAll(test_mongodb.setup);
@@ -5,6 +6,7 @@ afterAll(test_mongodb.teardown);
 
 const User = require("./User");
 
+// Test case for "User model"
 describe("User model", () => {
   const username = "kevin";
   const email = "kevin@example.com";
@@ -14,7 +16,7 @@ describe("User model", () => {
   it("can be saved", async () => {
     await expect(user.save()).resolves.toBe(user);
   });
-  
+
   it("can be searched by _id", async () => {
     let searchResult = await User.findById(user._id);
     expect(searchResult.username).toEqual(username);
@@ -45,5 +47,32 @@ describe("User model", () => {
     await user.remove();
     let searchResult = await User.findById(user._id);
     expect(searchResult).toBeNull();
+  });
+});
+
+// Test case for "Unique fields in User model"
+describe("Unique fields in User model", () => {
+  const username1 = "kevin";
+  const email1 = "kevin@example.com";
+
+  const username2 = "gordon";
+  const email2 = "gordon@example.com";
+
+  let user1 = new User({ username: username1, email: email1 });
+
+  beforeEach(async () => await user1.save());
+
+  it("should not allow two users with the same name", async () => {
+    let userWithSameName = new User({ username: username1, email: email2 });
+    await expect(userWithSameName.save()).rejects.toThrow(
+      "username: should be unique"
+    );
+  });
+
+  it("should not allow two users with the email", async () => {
+    let userWithSameEmail = new User({ username: username2, email: email1 });
+    await expect(userWithSameEmail.save()).rejects.toThrow(
+      "email: should be unique"
+    );
   });
 });
